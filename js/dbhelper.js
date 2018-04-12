b9beb3a7949fdc8bb3558701f74d4d21185a25f2
Andrey B <vozglas@gmail.com>
2018-04-12 17:41:40 +0300

    Change short_name in manifest

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -196,8 +196,7 @@ class DBHelper {
       title: restaurant.name,
       url: DBHelper.urlForRestaurant(restaurant),
       map: map,
-      animation: google.maps.Animation.DROP,
-    content: '<div style="width: 100px; height: 100px; background-color: red;">test</div>'}
+      animation: google.maps.Animation.DROP}
     );
     return marker;
   }
